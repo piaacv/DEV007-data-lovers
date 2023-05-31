9d61c5ad0fef0caa19123672485208df79cef218
Shantal Eslava <mseg006@gmail.com>
2023-05-31 11:43:11 -0600

    se agrego el selector y se creo base para el mismo en mainJs

--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import tarot from './data/tarot/tarot.js';
 
 
 const showCards = document.getElementById("showAllCards");
+const filer_nameaz= document.getElementById("filter_nameza")
+const filer_nameza= document.getElementById("filter_nameaz")
+const filer_acendente= document.getElementById("filter_acendente")
+const filter_decendente= document.getElementById("filter_decendente")
 const cards= tarot.cards;
+
 console.log(cards)
 
 cards.forEach(imageCards => {
     showAllCards.innerHTML += `
     <div><img src="${imageCards.img}"></div>`
 })
+
+const filterbutton = document.getElementById(filterbutton)
+filterbutton.addEventListener ("click", () =>{
+    if (filterbutton === filer_nameza){
+        showCards.innerHTML
+    }
+}
+ )
